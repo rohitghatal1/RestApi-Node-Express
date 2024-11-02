47a18e9dbacc18410656dcc532e3fa24d1a8232b
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-02 13:43:42 +0545

    added get request handler function

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const users = require('./MOCK_DATA.json');
 const app = express();
 const PORT = 8000;
 
-app.get("/api/users", (req, res)=> {
-    res.send(users);
+app.get("/users", (req, res) => {
+    const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`)}
+        </ul>
+    `;
+    res.send(html);
+
 })
+
+//REST API
+app.get("/api/users", (req, res)=> {
+    res.json(users);
+})
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+
 
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`))
