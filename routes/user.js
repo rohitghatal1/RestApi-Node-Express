acf269bb5ac844090d57ec1ec9ec24c7fe46c7f9
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-07 15:46:49 +0545

    added antd

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const {getAllUsers, getUserById, updateUserById, createUser} = require("../controllers/user")
+const { getAllUsers, getUserById, updateUserById, createUser } = require("../controllers/user")
 
 const router = express.Router()
 
-// router.get("/users", async (req, res) => {
-//     const allDBUsers = await User.find({});
-//     const html = `
-//         <ul>
-//             ${allDBUsers.map((user) => `<li>${user.first_name} - ${user.email}</li>`).join("")}
-//         </ul>
-//     `;
-//     res.send(html);
+router.get("/users", async (req, res) => {
+    const allDBUsers = await User.find({});
+    const html = `
+        <ul>
+            ${allDBUsers.map((user) => `<li>${user.first_name} - ${user.email}</li>`).join("")}
+        </ul>
+    `;
+    res.send(html);
 
-// })
+})
 
 
 router.route("/")
---
+++ views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div >
+            <div>
+                <h2></h2>
+            </div>
+        </div>
+    )
+}
